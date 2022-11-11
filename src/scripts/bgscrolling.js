13a2d5032a0810d33db6c6731b8967ad39590469
SysWhite <micsimfrayt@gmail.com>
2022-11-11 22:40:23 +0100

    .

--- src/scripts/bgscrolling.js
+++ src/scripts/bgscrolling.js
@@ -1,4 +1,4 @@
-const bgnum = Math.floor(Math.random() * 7);
+const bgnum = Math.floor(Math.random() * 6);
 document.getElementById("body").style.backgroundImage = `url('/src/assets/bgs/${bgnum}.png')`;
 setInterval(function() {
     const bgnum = Math.floor(Math.random() * 7);
--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,22 +1,31 @@
-var getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
-      }
+function gettemp() {
+    var getJSON = function(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function() {
+          var status = xhr.status;
+          if (status === 200) {
+            callback(null, xhr.response);
+          } else {
+            callback(status, xhr.response);
+          }
+        };
+        xhr.send();
     };
-    xhr.send();
-};
-getJSON('https://api.openweathermap.org/data/2.5/weather?lat=46.4892648&lon=11.3296481&appid=4790a5688fb51270e09ea7bb403b8215',
-function(err, data) {
-  if (err !== null) {
-    alert('Something went wrong: ' + err);
-  } else {
-    document.getElementById("temp").innerText = (data.main.feels_like - 273.15).toFixed(0) + "°C";
-  }
-});
+    getJSON('https://api.openweathermap.org/data/2.5/weather?lat=46.4892648&lon=11.3296481&appid=4790a5688fb51270e09ea7bb403b8215',
+    function(err, data) {
+      if (err !== null) {
+        alert('Something went wrong: ' + err);
+      } else {
+        document.getElementById("temp").innerText = (data.main.feels_like - 273.15).toFixed(0) + "°C";
+      }
+    });
+
+}
+
+gettemp();
+setInterval(function() {
+    gettemp();
+}, 60 * 1000);
+
